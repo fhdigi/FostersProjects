95812025bae91aceaabcf23282e1be705fa41db3
Fred Heidrich <fred.heidrich@gmail.com>
2018-06-08 17:27:49 -0400

    Added in the viewmodels

---
+++ FostersDisposal/MailingLabelApp/ViewModels/LcBaseViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MailingLabelApp.ViewModels
+{
+    public class LcBaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Sets the property.
+        /// </summary>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+        /// <param name="backingStore">Backing store.</param>
+        /// <param name="value">Value.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <param name="onChanged">On changed.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        protected virtual bool SetProperty<T>(
+            ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the property changed event.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public class CommandHandler : ICommand
+    {
+        private readonly Action _action;
+        private readonly bool _canExecute;
+
+        public CommandHandler(Action action, bool canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
--- FostersDisposal/MailingLabelApp/ViewModels/MainWindowViewModel.cs
+++ FostersDisposal/MailingLabelApp/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using MailingLabelApp.Models;
 
-namespace MailingLabelApp
+namespace MailingLabelApp.ViewModels
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel : LcBaseViewModel
     {
+        private ObservableCollection<Customer> _customerListing;
+        private bool _showMonday;
+        private bool _showTuesday;
+        private bool _showWednesday;
+        private bool _showThursday;
+        private bool _showFriday;
+
+        public ObservableCollection<Customer> CustomerListing
+        {
+            get => _customerListing;
+            set => SetProperty(ref _customerListing, value);
+        }
+
+        public bool ShowMonday
+        {
+            get => _showMonday;
+            set => SetProperty(ref _showMonday, value);
+        }
+
+        public bool ShowTuesday
+        {
+            get => _showTuesday;
+            set => SetProperty(ref _showTuesday, value);
+        }
+
+        public bool ShowWednesday
+        {
+            get => _showWednesday;
+            set => SetProperty(ref _showWednesday, value);
+        }
+
+        public bool ShowThursday
+        {
+            get => _showThursday;
+            set => SetProperty(ref _showThursday, value);
+        }
+
+        public bool ShowFriday
+        {
+            get => _showFriday;
+            set => SetProperty(ref _showFriday, value);
+        }
+
+        public void RefreshListing()
+        {
+            CustomerListing = new ObservableCollection<Customer>(Customer.GetCustomers());
+        }
     }
 }
