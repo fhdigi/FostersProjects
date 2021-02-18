0a5f8a9c7a2fd0f4c1176d1a4203958aac87b27f
Fred Heidrich <fred.heidrich@gmail.com>
2021-02-18 09:35:24 -0500

    Balancing program

---
+++ FostersDisposal/FosterBalancer/FosterBalancer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FosterBalancer
+{
+    class Program
+    {
+        private static string DatabaseConnection = "";
+
+        static void Main(string[] args)
+        {
+            // Local Machine 
+            // DatabaseConnection = @"Data Source=LCCSWORK002\SQLEXPRESS;Initial Catalog=DisposalData;Integrated Security=True";
+            
+            // Server at Office 
+            DatabaseConnection = @"Data Source=DESKTOP-JE8H0DC\SQLEXPRESS;Initial Catalog=DisposalData;Integrated Security=True";
+
+            // First run the commercial balance routine 
+            RunBalanceRoutineCommercial();
+
+            // Next run the residential balance routine 
+            RunBalanceRoutineResidential();
+        }
+
+        private static void RunBalanceRoutineCommercial()
+        {
+            try
+            {
+                PickupTransaction.RentalCustomer.SetCustomerBalances(DatabaseConnection);
+            }
+            catch 
+            {
+            }
+        }
+
+        private static void RunBalanceRoutineResidential()
+        {
+            try
+            {
+                PickupTransaction.Customer.SetCustomerBalances(DatabaseConnection);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
